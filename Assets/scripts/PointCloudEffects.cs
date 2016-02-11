b6bb73e03a252a452d4a14d12f70dd1ed3f3a016
blackboxfcsh <blackbox.team@fcsh.unl.pt>
2016-02-11 18:06:34 +0000

    * added first version of PointCloudEffects Class

---
+++ Assets/scripts/PointCloudEffects.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointCloudEffects : MonoBehaviour {
+
+
+	GameObject[] clustersGameObjects;
+
+	// Use this for initialization
+	void Start () {
+	
+		clustersGameObjects = GameObject.FindGameObjectsWithTag("Cluster");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+	}
+
+	void DrawLines(){
+	
+		
+	}
+}
